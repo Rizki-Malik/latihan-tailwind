51689a2579877bceb5031bb9bb64a2b65c6bb20c
Muhammad Rizki Malik Aziz <rizkimalik221001@gmail.com>
2024-02-06 13:29:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+         colors:{
+          wpu:'#bada55',
+        },
+         animation: {
+         'spin-slow': 'spin 3s linear infinite',
+         'wiggle': 'wiggle 1s ease-in-out infinite'
+       },
+       
+        keyframes: {
+          wiggle: {
+            '0%, 100%': {
+              transform: 'rotate(-3deg)'
+            },
+            '50%': {
+              transform: 'rotate(3deg)'
+            },
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
